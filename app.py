7a325e4569d8586ca4fa25493b1f36338e1c0ae9
estringfellow <stringfellow.eric@gmail.com>
2020-05-26 16:13:02 -0400

    5.26.20 script consolidation

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from gensim.test.utils import common_corpus, common_dictionary, get_tmpfile
 from datetime import date
 from read_functions import extract_text_from_docx, extract_text_from_pdf, extract_text_from_doc
-import io
+import io, sys, PyPDF2
+
 
 # Get the current date for the filename
 today = date.today()
@@ -26,165 +27,186 @@ def get_doc_files():
     all_doc_files = [file for file in glob.glob(DATA_DIR + "\*.doc*")]
     return all_doc_files
 
-# DOCX files
-def tokenize_docx_files(docx_filename):
-    docx_file_docs = []
-    docx_str = extract_text_from_docx(DATA_DIR + docx_filename)
-    docx_sent_tokenized = sent_tokenize(docx_str)
-    for line in docx_sent_tokenized:
-        docx_file_docs.append(line)
 
-    # Tokenize words for each sentence
-    docx_gen_docs = [[w.lower() for w in word_tokenize(text)] for text in docx_file_docs]
-    
-    return docx_gen_docs
-
-
+# PDF files
 def tokenize_pdf_files(pdf_filename):
     file_text_pdf = str()
     pdf_file_docs = []
-    pdf_file = extract_text_from_pdf(pdf_filename)
-    
-    for page in pdf_file:
+    pdf_file_gen_obj = extract_text_from_pdf(pdf_filename)
+
+    # Loop through pages in pdf generator object
+    for page in pdf_file_gen_obj:
         file_text_pdf += ' ' + page
         file_text_pdf = file_text_pdf.lower()
-        
+
     pdf_sent_tokenized = sent_tokenize(file_text_pdf)
 
     for line in pdf_sent_tokenized:
         pdf_file_docs.append(line)
         
-    # Tokenize words for each sentence
-    pdf_gen_docs = [[w.lower() for w in word_tokenize(text)] for text in pdf_file_docs]   
-        
+    # Tokenize & process words in each sentence
+    pdf_gen_docs = [gensim.utils.simple_preprocess(text, min_len=3) for text in pdf_file_docs]   
+
     return pdf_gen_docs
  
- 
-def tokenize_doc_files(doc_filename):
-    file_text_doc = str()
-    doc_file_docs = []
-    doc_file = extract_text_from_doc(doc_filename)
-    
-    doc_sent_tokenized = sent_tokenize(doc_file)  
-    
-    for line in doc_sent_tokenized:
-        doc_file_docs.append(line)
-        
-    # Tokenize words for each sentence
-    doc_gen_docs = [[w.lower() for w in word_tokenize(text)] for text in doc_file_docs]   
-        
-    return doc_gen_docs   
-    
-# Get list of documents in location #1
-
-# Get list of documents in location #2
-
-# Function to get list of documents in a provided directory
-def get_docs(location= os.curdir):
-    dir_files = [file for file in glob.glob(DATA_DIR + "\*.txt*")]
-    return dir_files
-
 
 
 if __name__ == '__main__':
-    import sys
+    print(sys.argv)
     
     if len(sys.argv) != 3:
         print('Incorrect number of parameters entered. Please refer to the README on the input parameters.')
         exit()
+    # elif FileNotFoundError:
+    #     print('Please check filenames and that docs are located in the ''data'' folder')
+    #     print('One or both files were not found.')
+    #     exit()
     else:
         doc_1 = sys.argv[1]
         doc_2 = sys.argv[2]
-
-# print(f"DOC FILES: {get_doc_files()}")
-# print(f"PDF FILES: {get_pdf_files()}")
-# print(f"DOCX FILES: {get_docx_files()}")
         
-file_docs = []
+        # Get filenames and file extensions of the input docs
+        doc_1_filename, doc_1_ext = os.path.splitext(sys.argv[1])[0], os.path.splitext(sys.argv[1])[1]
+        doc_2_filename, doc_2_ext = os.path.splitext(sys.argv[2])[0], os.path.splitext(sys.argv[2])[1]
+        
+    file_docs = []
+    
+    # Determine file extensions
+    # If .txt file:
+    if doc_1_ext == '.txt':
+        print('.txt file detected ...')
+        with open(DATA_DIR + '\\' + doc_1) as f:
+            tokens = sent_tokenize(f.read())
+            for line in tokens:
+                file_docs.append(line)
 
-doc_1 = 'similarity_demofile.txt'
-doc_2 = 'similarity_demofile2.txt'
+    # If .pdf file:
+    elif doc_1_ext == '.pdf':
+        pdfFileObj = open(DATA_DIR + '//' + doc_1, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        num_pages = pdfReader.numPages
+        # Loop through pages and append to list
+        for x in range(num_pages):
+            pageObj = pdfReader.getPage(x)
+            pdf_page = pageObj.extractText()
+            file_docs.append(pdf_page)
+    else:
+        raise TypeError
+        print('A non-txt and pdf file detected. Please use only .txt or .pdf files')
+        exit()
+    
+    
+    # Tokenize (and process) words for each sentence
+    gen_docs = [gensim.utils.simple_preprocess(text, min_len=3) for text in file_docs]
+    
+    print(gen_docs)
 
-# Open the document file
-with open(DATA_DIR + '\\' + doc_1) as f:
-    tokens = sent_tokenize(f.read())
-    for line in tokens:
-        file_docs.append(line)
+    print("Number of documents (sentences) in first file:", len(gen_docs))
 
-print("Number of documents (sentences) in first file:", len(file_docs))
+    # Create gensim dictionary with ID as the key, and word token as the value
+    dictionary = gensim.corpora.Dictionary(gen_docs)
+    # print(dictionary.token2id)
 
-# Tokenize (and process) words for each sentence
-# gen_docs = [[w.lower() for w in word_tokenize(text)] for text in file_docs]
-gen_docs = [gensim.utils.simple_preprocess(text) for text in file_docs]
+    ### Create a bag of words corpus, passing the tokenized list of words to the Dictionary.doc2bow()
+    corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
 
-# Create gensim dictionary with ID as the key, and word token as the value
-dictionary = gensim.corpora.Dictionary(gen_docs)
-# print(dictionary.token2id)
+    # TFIDF - down weights tokens (words) that appears frequently across documents
+    # Words that occur more frequently across the documents get smaller weights
+    tf_idf = gensim.models.TfidfModel(corpus)
 
-### Create a bag of words corpus, passing the tokenized list of words to the Dictionary.doc2bow()
-corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
+    # Print token_id and the token frequency
+    # for doc in tf_idf[corpus]:
+    #     print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
 
-# TFIDF - down weights tokens (words) that appears frequently across documents
-# Words that occur more frequently across the documents get smaller weights
-tf_idf = gensim.models.TfidfModel(corpus)
+    # Create index tempfile
+    index_tmpfile = get_tmpfile("wordindex")
 
-# Print token_id and the token frequency
-# for doc in tf_idf[corpus]:
-#     print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
+    # Create similarity measure object
+    sims = gensim.similarities.Similarity(index_tmpfile,tf_idf[corpus],
+                                            num_features=len(dictionary))
 
-# Create index tempfile
-index_tmpfile = get_tmpfile("wordindex")
+    # Create Query Document - how similar is this query document to each document in the index
+    file2_docs = []
 
-# Create similarity measure object
-sims = gensim.similarities.Similarity(index_tmpfile,tf_idf[corpus],
-                                        num_features=len(dictionary))
+    # Determine file extensions
+    # If .txt file:
+    if doc_2_ext == '.txt':
+        print('.txt file detected ...')
+        with open(DATA_DIR + '\\' + doc_2) as f:
+            tokens = sent_tokenize(f.read())
+            for line in tokens:
+                file2_docs.append(line)
+                # tokenize & process words
 
-# Create Query Document - how similar is this query document to each document in the index
-file2_docs = []
+    # If .pdf file:
+    elif doc_2_ext == '.pdf':
+        print('pdf file detected ...')
+        pdfFileObj = open(DATA_DIR + '//' + doc_2, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        num_pages = pdfReader.numPages
+        # Loop through pages and append to list
+        for x in range(num_pages):
+            pageObj = pdfReader.getPage(x)
+            pdf_page = pageObj.extractText()
+            file2_docs.append(pdf_page)
+        
+        # file2_docs = tokenize_pdf_files(DATA_DIR + '\\' + doc_2)
+    else:
+        raise TypeError('A non-txt and pdf file detected. Please use only .txt or .pdf files')
+        exit()
 
-# Open the document file
-with open(DATA_DIR + '\\' + doc_2) as f:
-    tokens = sent_tokenize(f.read())
-    for line in tokens:
-        file2_docs.append(line)
+    print("Number of documents (sentences) in second file:", len(file2_docs))
+    
 
-print("Number of documents (sentences) in second file:", len(file2_docs))
+    # Array of averages (len = number of docs in the query)
+    # Each entry in the list is the average similarity of the docs in the query doc compared to the corpus
+    avg_sims = [] 
+    
+    # query_doc_bow = dictionary.doc2bow(query_doc)
+    # # find similarity for each document
+    # query_doc_tf_idf = tf_idf[query_doc_bow]
+    # # print (document_number, document_similarity)
+    # print('Comparing Result:', sims[query_doc_tf_idf]) 
+    # # calculate sum of similarities for each query doc
+    # sum_of_sims =(np.sum(sims[query_doc_tf_idf], dtype=np.float32))
+    # # calculate average of similarity for each query doc
+    # avg = sum_of_sims / len(file_docs)
+    # # print average of similarity for each query doc
+    # print(f'avg: {sum_of_sims / len(file_docs)}')
+    # # add average values into array
+    # avg_sims.append(avg)  
 
-# Array of averages (len = number of docs in the query)
-# Each entry in the list is the average similarity of the docs in the query doc compared to the corpus
-avg_sims = [] 
+    for line in file2_docs:
+        # tokenize & process words
+        query_doc = gensim.utils.simple_preprocess(line, min_len=3)
+        # query_doc = gensim.utils.simple_preprocess(line, min_len=3)
+        # create bag of words
+        query_doc_bow = dictionary.doc2bow(query_doc)
+        # find similarity for each document
+        query_doc_tf_idf = tf_idf[query_doc_bow]
+        # print (document_number, document_similarity)
+        print('Comparing Result:', sims[query_doc_tf_idf]) 
+        # calculate sum of similarities for each query doc
+        sum_of_sims =(np.sum(sims[query_doc_tf_idf], dtype=np.float32))
+        # calculate average of similarity for each query doc
+        avg = sum_of_sims / len(file_docs)
+        # print average of similarity for each query doc
+        print(f'avg: {sum_of_sims / len(file_docs)}')
+        # add average values into array
+        avg_sims.append(avg)  
 
+    print(query_doc)
+    print('AVERAGE SIMS: ' , avg_sims)    
 
-# for line in query documents
-for line in file2_docs:
-    # tokenize & process words
-    query_doc = gensim.utils.simple_preprocess(line)
-    # create bag of words
-    query_doc_bow = dictionary.doc2bow(query_doc)
-    # find similarity for each document
-    query_doc_tf_idf = tf_idf[query_doc_bow]
-    # print (document_number, document_similarity)
-    print('Comparing Result:', sims[query_doc_tf_idf]) 
-    # calculate sum of similarities for each query doc
-    sum_of_sims =(np.sum(sims[query_doc_tf_idf], dtype=np.float32))
-    # calculate average of similarity for each query doc
-    avg = sum_of_sims / len(file_docs)
-    # print average of similarity for each query doc
-    print(f'avg: {sum_of_sims / len(file_docs)}')
-    # add average values into array
-    avg_sims.append(avg)  
+    # calculate total average
+    total_avg = ((np.sum(avg_sims, dtype=np.float)) / len(file2_docs))
 
-print('AVERAGE SIMS: ' , avg_sims)    
+    # round the value and multiply by 100 to format it as percentage
+    percentage_of_similarity = round(float(total_avg) * 100)
+    print('Similarity percentage: ' , percentage_of_similarity)
 
-# calculate total average
-total_avg = ((np.sum(avg_sims, dtype=np.float)) / len(file2_docs))
-
-# round the value and multiply by 100 to format it as percentage
-percentage_of_similarity = round(float(total_avg) * 100)
-print('Similarity percentage: ' , percentage_of_similarity)
-
-# if percentage is greater than 100
-if percentage_of_similarity >= 100:
-	percentage_of_similarity = 100
+    # if percentage is greater than 100
+    if percentage_of_similarity >= 100:
+        percentage_of_similarity = 100
 
 
--- read_functions.py
+++ read_functions.py
@@ -4,8 +4,13 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-import io
-import textract, spacy, re
+import io, glob
+import textract, spacy, re, os
+from nltk import word_tokenize, sent_tokenize
+
+# Directory variables (root dir, data dir, etc.)
+ROOT_DIR = os.path.abspath(os.curdir)
+DATA_DIR = os.path.join(os.path.abspath(os.curdir), 'data')
 
 # Function used to extract data from .docx files
 def extract_text_from_docx(doc_path):
@@ -75,4 +80,36 @@ def extract_text_from_doc(doc_filename):
     #         skillset.append(token)
     
     return output_string
+
+def tokenize_doc_files(doc_filename):
+    file_text_doc = str()
+    doc_file_docs = []
+    doc_file = extract_text_from_doc(doc_filename)
     
+    doc_sent_tokenized = sent_tokenize(doc_file)  
+    
+    for line in doc_sent_tokenized:
+        doc_file_docs.append(line)
+        
+    # Tokenize words for each sentence
+    doc_gen_docs = [[w.lower() for w in word_tokenize(text)] for text in doc_file_docs]   
+        
+    return doc_gen_docs   
+    
+# Function to get list of documents in a provided directory
+def get_docs(location= os.curdir):
+    dir_files = [file for file in glob.glob(DATA_DIR + "\*.txt*")]
+    return dir_files
+
+# DOCX files
+def tokenize_docx_files(docx_filename):
+    docx_file_docs = []
+    docx_str = extract_text_from_docx(DATA_DIR + docx_filename)
+    docx_sent_tokenized = sent_tokenize(docx_str)
+    for line in docx_sent_tokenized:
+        docx_file_docs.append(line)
+
+    # Tokenize words for each sentence
+    docx_gen_docs = [[w.lower() for w in word_tokenize(text)] for text in docx_file_docs]
+    
+    return docx_gen_docs
